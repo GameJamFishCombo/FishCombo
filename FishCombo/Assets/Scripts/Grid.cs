2021c0e53043dfbebdadb5d86256911b3b344e98
Julian RaShaun Gay <jrg180004@utdallas.edu>
2022-03-11 23:36:33 -0600

    Created grid

---
+++ FishCombo/Assets/Scripts/Grid.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [Header("Art Assets")]
+    [SerializeField] private Material tileMaterial;
+
+    private const int TILE_COUNT_X = 8;
+    private const int TITLE_COUNT_Y = 4;
+    private GameObject[,] tiles;
+    private Camera currentCamera;
+    private Vector2Int currHover;
+
+    private void Awake() {
+        GenerateAllTiles(1, TILE_COUNT_X, TITLE_COUNT_Y);
+    }
+
+    public void Update() {
+        if(!currentCamera) {
+            currentCamera = Camera.main;
+            return;
+        }
+
+        RaycastHit info;
+        Ray ray = currentCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out info, 100, LayerMask.GetMask("Tile", "Hover"))) {
+            //get the indexes of the tile i've hit
+            Vector2Int hitPos = LookupTileIndex(info.transform.gameObject);
+
+            if(currHover == -Vector2Int.one) {
+                currHover = hitPos;
+                tiles[hitPos.x, hitPos.y].layer = LayerMask.NameToLayer("Hover");
+            }
+
+            if(currHover != hitPos) {
+                tiles[currHover.x, currHover.y].layer = LayerMask.NameToLayer("Tile");
+                currHover = hitPos;
+                tiles[hitPos.x, hitPos.y].layer = LayerMask.NameToLayer("Hover");
+            }
+        } else {
+            if(currHover != -Vector2Int.one) {
+                tiles[currHover.x, currHover.y].layer = LayerMask.NameToLayer("Tile");
+                currHover = -Vector2Int.one;
+            }
+        }
+    }
+
+    private void GenerateAllTiles(float tileSize, int tileCountX, int tileCountY) {
+        tiles = new GameObject[tileCountX, tileCountY];
+
+        for (int x = 0; x < tileCountX; x++) { 
+            for (int y = 0; y < tileCountY; y++) {
+                tiles[x,y] = GenerateSingleTile(tileSize, x, y);
+            }
+        }
+    }
+
+    private GameObject GenerateSingleTile(float tileSize, int x, int y) {
+        GameObject tileObj = new GameObject(string.Format("X:{0} Y:{1}", x, y));
+        tileObj.transform.parent = transform;
+
+        Mesh mesh = new Mesh();
+        tileObj.AddComponent<MeshFilter>().mesh = mesh;
+        tileObj.AddComponent<MeshRenderer>().material = tileMaterial;
+
+        Vector3[] vertices = new Vector3[4];
+        vertices[0] = new Vector3(x * tileSize, 0, y * tileSize);
+        vertices[1] = new Vector3(x * tileSize, 0, (y+1) * tileSize);
+        vertices[2] = new Vector3((x+1) * tileSize, 0, y * tileSize);
+        vertices[3] = new Vector3((x+1) * tileSize, 0, (y+1) * tileSize);
+
+        int[] tris = new int[] {0, 1, 2, 1, 3, 2};
+
+        mesh.vertices = vertices;
+        mesh.triangles = tris;
+        mesh.RecalculateNormals();
+
+        tileObj.layer = LayerMask.NameToLayer("Tile");
+        tileObj.AddComponent<BoxCollider>();
+
+        return tileObj;
+    }
+
+    private Vector2Int LookupTileIndex(GameObject hitInfo) {
+        for (int x = 0; x < TILE_COUNT_X; x++) { 
+            for (int y = 0; y < TITLE_COUNT_Y; y++) {
+                if (tiles[x,y] == hitInfo) {return new Vector2Int(x,y);}
+            }
+        }
+
+        return -Vector2Int.one; //-1 -1; Invalid
+    }
+}
