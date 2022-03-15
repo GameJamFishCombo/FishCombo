85570ff6960b9b8b0748ff550b2d11c4cb5e98fd
Jarod Shelton <jarodshelton01@gmail.com>
2022-03-15 00:13:50 -0500

    Update Player.cs
    Added input buffering

--- FishCombo/Assets/Scripts/Player.cs
+++ FishCombo/Assets/Scripts/Player.cs
@@ -11,8 +11,11 @@ public class Player : Units
     Rigidbody rigidbody;
     bool canMove = true;
     public float projectileSpeed = 450;
+
+    private Queue<MovementInput> buffer;
     void Awake()
     {
+        buffer = new Queue<MovementInput>();
         rigidbody = GetComponent<Rigidbody>();
         player = GetComponent<Transform>();
         playerPos = rigidbody.position;
@@ -20,52 +23,72 @@ public class Player : Units
 
     public void Update() {
 
-        if(canMove) {        
+        if(Input.GetKeyDown(KeyCode.UpArrow)) {
+            if(buffer.Count < 3)
+                buffer.Enqueue(MovementInput.Up);
+        }
 
-            if(Input.GetKeyDown(KeyCode.W)) {
+        if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+            if(buffer.Count < 3)
+                buffer.Enqueue(MovementInput.Left);
+        }
+            
+        if(Input.GetKeyDown(KeyCode.DownArrow)) {
+            if(buffer.Count < 3)
+                buffer.Enqueue(MovementInput.Down);
+        }
+
+        if(Input.GetKeyDown(KeyCode.RightArrow)) {
+            if(buffer.Count < 3)
+                buffer.Enqueue(MovementInput.Right);
+        }
+
+        if(Input.GetKeyDown(KeyCode.C) && canMove) //if between tiles, round up or down
+        {
+            Launch();
+        }
+
+        move();
+
+        if(Input.GetKeyDown(KeyCode.K)) {
+            TakeDmg(5);
+        }
+    }
+
+    private void move(){
+        if(canMove && buffer.Count > 0) {        
+            MovementInput input = buffer.Dequeue();
+            if(input == MovementInput.Up) {
                 Vector3 move = new Vector3(0, 0, 1f) + player.position;
-
                 if(!(move.x < 0 || move.z < 0 || move.x > 3 || move.z > 3)) {
                     StartCoroutine(LerpPosition(move, duration));
                 }
 
             }
 
-            if(Input.GetKeyDown(KeyCode.A)) {
+            if(input == MovementInput.Left) {
                 Vector3 move = new Vector3(-1f, 0, 0) + player.position;
-
                 if(!(move.x < 0 || move.z < 0 || move.x > 3 || move.z > 3)) {
                     StartCoroutine(LerpPosition(move, duration));
                 }
 
             }
             
-            if(Input.GetKeyDown(KeyCode.S)) {
+            if(input == MovementInput.Down) {
                 Vector3 move = new Vector3(0, 0, -1f) + player.position;
-
                 if(!(move.x < 0 || move.z < 0 || move.x > 3 || move.z > 3)) {
                     StartCoroutine(LerpPosition(move, duration));
                 }
 
             }
 
-            if(Input.GetKeyDown(KeyCode.D)) {
+            if(input == MovementInput.Right) {
                 Vector3 move = new Vector3(1f, 0, 0) + player.position;
-
                 if(!(move.x < 0 || move.z < 0 || move.x > 3 || move.z > 3)) {
                     StartCoroutine(LerpPosition(move, duration));
                 }
 
             }
-
-            if(Input.GetKeyDown(KeyCode.L)) //if between tiles, round up or down
-            {
-                Launch();
-            }
-        }
-
-        if(Input.GetKeyDown(KeyCode.K)) {
-            TakeDmg(5);
         }
     }
 
@@ -101,4 +124,12 @@ public class Player : Units
         Destroy(this.gameObject);
         Debug.Log(player + " dead.");
     }
+}
+
+public enum MovementInput
+{
+    Up = 0,
+    Left = 1,
+    Down = 2,
+    Right = 3,
 }
