343eecce59f7a6a581dde479bc84d56e11e6e2ba
Whalics <91243745+Whalics@users.noreply.github.com>
2022-03-19 14:05:12 -0500

    Cutscen scne
    yes

--- FishCombo/Assets/Scripts/UI/Cutscene.cs
+++ FishCombo/Assets/Scripts/UI/Cutscene.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Cutscene : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    public Animator[] animators;
+    public AudioSource paper;
+    public int sceneNum = 0;
+    public float timer = 3;
+    public float time;
+
+    public float exitTimer;
+    public float exitTime;
+
+    public GameObject transitionOut;
+    public bool cutsceneOver;
+    public string levelToLoad;
+    void Start(){
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    void Update(){
+        if(cutsceneOver)
+            exitTime -= Time.deltaTime;
+
+        time-=Time.deltaTime;
+        if(time <=0){
+            time = timer;
+            sceneNum++;
+            PlayScene();
+        }
+        if(exitTime <=0){
+            SceneManager.LoadScene(levelToLoad);
+        }
+
     }
+    void PlayScene(){
+        if(sceneNum < animators.Length)
+            animators[sceneNum].Play("In");
+            else
+            transitionOut.SetActive(true);
+            cutsceneOver = true;
+    }
+
+    void Skipscene(){
+        time = 0;
+    }
+
+    
 }
