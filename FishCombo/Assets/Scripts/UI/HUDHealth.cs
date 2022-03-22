dd727ea2561514b104fb4b26e3586c249ef2ac18
Whalics <91243745+Whalics@users.noreply.github.com>
2022-03-22 17:56:45 -0500

    Healthbar Animation
    -healthbars wiggle on damage taken

--- FishCombo/Assets/Scripts/UI/HUDHealth.cs
+++ FishCombo/Assets/Scripts/UI/HUDHealth.cs
@@ -10,6 +10,7 @@ public class HUDHealth : MonoBehaviour
     bool lerpHP = false;
     float time = 0;
     public int health;
+    public Animator animator;
     void Start() {
         SetMaxHealth(health);
     }
@@ -21,7 +22,13 @@ public class HUDHealth : MonoBehaviour
 
     public void SetHealth(float health, float originalHP) {
         time = 0;
-
+        if(animator !=null){
+            //if(gameObject.tag == "Player")
+            animator.Play("HealthWiggle",-1,0.0f);
+            // if(gameObject.tag == "Enemy"){
+            //     animator.Play("HealthGrow",-1,0.0f);  
+            // }
+        }
         if(!lerpHP)
             StartCoroutine(LerpHP(health, originalHP));
 
