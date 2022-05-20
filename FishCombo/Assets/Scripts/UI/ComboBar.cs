4c2d6effd8896c7c59cabd6d73abffe7d3c7d0f5
Whalics <91243745+Whalics@users.noreply.github.com>
2022-05-20 18:26:19 -0500

    Lerp
    Lerp

--- FishCombo/Assets/Scripts/UI/ComboBar.cs
+++ FishCombo/Assets/Scripts/UI/ComboBar.cs
@@ -9,6 +9,10 @@ public class ComboBar : MonoBehaviour
     Animator animator;
     public float lerpSpd = 1;
     float time = 0;
+    public float speed = 2;
+    public float newCombo;
+    public float oldCombo;
+    public float timeStartedLerping;
 
     void Start(){
         animator = gameObject.GetComponent<Animator>();
@@ -21,17 +25,36 @@ public class ComboBar : MonoBehaviour
     public void SetCombo(float comboNew, float comboOld) {
         // slider.value = combos;
         time = 0;
-
-        StartCoroutine(LerpCombo(comboNew, comboOld));
+        newCombo = comboNew;
+        oldCombo = comboOld;
+        timeStartedLerping = Time.time;
+        //StartCoroutine(LerpCombo(comboNew, comboOld));
     }
 
-    IEnumerator LerpCombo(float newCombo, float oldCombo) {
-        while(time < 1) {
-            time += (lerpSpd * Time.deltaTime);
-            slider.value = Mathf.Lerp(oldCombo, newCombo, time);
-            yield return null;
-        }
-    }   
+    void Update(){
+        LerpComboValue();
+    }
+
+
+    // IEnumerator LerpCombo(float newCombo, float oldCombo) {
+    //     while(time < 1) {
+    //         time += (lerpSpd * Time.deltaTime);
+    //         slider.value = Mathf.Lerp(oldCombo, newCombo, time);
+    //         if(newinputqueued){
+    //             StartCoroutine(LerpCombo(newcombo,slider.value);
+    //             StopCoroutine(LerpCombo());
+    //             StopCoroutine(LerpCombo();
+    //         }
+    //         yield return null;
+    //     }
+    // }
+    
+    public void LerpComboValue(){
+        
+        float timeSinceStarted = Time.time - timeStartedLerping;
+        float percentageComplete = timeSinceStarted / speed;
+        slider.value = Mathf.Lerp(slider.value, newCombo, percentageComplete); //Time.deltaTime * speed
+    }
 
     public void PlayAnimation(string animation){
         animator.Play(animation);
